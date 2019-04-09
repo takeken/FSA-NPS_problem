fe9b78cd086e9013893d3165530c1f9704a8435b
takeda <takeken7180@gmail.com>
2019-04-09 21:46:37 +0900

    major modify algorithm

--- polynomial_alg.py
+++ polynomial_alg.py
@@ -7,72 +7,92 @@ import copy
 #import topology
 
 
-cost_ls = []
+#sys.setrecursionlimit(100)
 b = 150
+rho = 1
 N = 3
+M = 1
 eta = [50,25,25]
-hop = [1,2,3]
+hop = [1,2,2]
 
 
-#全ての割り当てパターンとそのコストをリスト化
-def make_list(b,hop,N):
-  
-  B_max = math.ceil(b/min(eta))    #パスに割り当てるスロット数の上限を定める
-  
-  #割り当てを全て書き出す。
-  Bk_ls = [a+1 for a in range(B_max)]
-  Bk_comb =list(itertools.product(Bk_ls, repeat=N))
-  
-  #各割り当て時の使用スペクトル資源をリスト化
-  for a in Bk_comb:
-    cost = 0
-    for k in range(N):
-      cost = a[k]*hop[k] + cost
-    cost_ls.append(cost)
-    
-  return cost_ls,Bk_comb
 
 
-#コスト順にソートした時のインデックスを取得
-def sort_list(cost_ls):
-  sort_index = sorted(range(len(cost_ls)), key=lambda k: cost_ls[k])
-  return sort_index
+# コストを計算
+def cal_cost(Bk,hop):
+  Bkhop = [x*y for (x,y) in zip(Bk,hop)]
+  cost = sum (Bkhop)
+  return cost
+
   
   
-#ソート後のインデックス順に要求を満たすか確認
-def check(a,Bk_comb,eta,N,b,M):
+
+#要求を満たすか確認
+def check(Bk,eta,N,b,rho,M):
   b_sum = 0
-  Bk = Bk_comb[a]
   
   bk = [Bk[k]*eta[k] for k in range(N)]
+  print(Bk,bk)
   b_sum = sum(bk)   #通常時の伝送容量
-    
-  if b_sum < b:
-    return 0
+  abc = (b_sum-sum(sorted (bk, reverse = True)[:M]))
+  print(abc)
+  if (b_sum >= b and b_sum -  sum (sorted (bk, reverse = True)[:M]) >= rho*b):  ##合計伝送容量が足りない or M本故障時の伝送容量
+    return 1      #故障時に耐えれない
   else:
-    if b_sum -  sum (sorted (bk, reverse = True)[:M]) < 0:  #M本故障時の伝送容量
-      return 0
-    else:
-      return 1
+    return 0  #トラヒック要求を満たす
+
+
+
+#割り当てを決める
+def allocation(Bk,eta,hop,N,b,rho,M,n):
+  global cost
+  global opt_Bk
+  if (n == N - 1):
+    # ネストの最深部手前まできている場合
+    for i in range(1, B_max+1):
+      Bk[n] = i
+        
+      if check(Bk,eta,N,b,rho,M) == 1:   # Bkが要求を満たすか調べる
+        if cost >= cal_cost(Bk,hop):    # コストを比較し小さければ更新
+          cost = cal_cost(Bk,hop)
+          opt_Bk = Bk
+          #print(Bk)
+          #print(cost)
+        return
+        
+    return
+  else:
+    for i in range(1, B_max+1):
+      Bk[n] = i
+      allocation(Bk,eta,hop,N,b,rho,M,n+1)
+    return
+            
+
+        
     
-  
+
+
+
 
 
 if __name__ == "__main__":
+  B_max = math.ceil(b/min(eta))    #パスに割り当てるスロット数の上限を定める
+  Bk = [0] * N      #Bkの初期設定
   
-  cost_ls, Bk_comb = make_list(100,hop,N)
-  #print(cost_ls)
-  sort_index = sort_list(cost_ls)
-  #print(sort_index)
-  #print(len(cost_ls))
-  #print(Bk_comb[sort_index[1]])
+  cost = math.inf
   
-  for index in sort_index:
-    if check(index,Bk_comb,eta,N,b,1) == 1:
-      ans_index = index
-      break
   
-  print(Bk_comb[ans_index])
+  allocation(Bk,eta,hop,N,b,rho,M,0)
+  print(cost)
+  
+          
+        
+      
+    
+    
+    
+  
+
   
   
     
